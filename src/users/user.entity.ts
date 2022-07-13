709e7c6a7bc5855428f3beb3a6cc6fb89d1a453c
Devendra <devendragupta1302@gmail.com>
2022-07-13 17:54:33 +0530

    Update

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,29 @@
-import { Entity,Column, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert,AfterUpdate,AfterRemove,Entity,Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class User {
+export class User{
     @PrimaryGeneratedColumn()
     id: number;
 
     @Column()
-    email:string;
+    email: string;
     
     @Column()
     password:string;
+
+    @AfterInsert()
+    logInsert(){
+        console.log("Inserted user with id",this.id);
+        
+    }
+    @AfterUpdate()
+    logUpdate(){
+        console.log("Updated user with id",this.id);
+        
+    }
+    @AfterRemove()
+    logRemove(){
+        console.log("Removed user with id",this.id);
+        
+    }
 }
